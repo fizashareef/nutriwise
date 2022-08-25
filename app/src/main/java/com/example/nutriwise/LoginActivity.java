e6fff9f582dd2b9b5ef50862ba4591cc70265d72
Fizashareef <fizashareef01@gmail.com>
2022-08-25 21:47:05 +0530

    iniit updated

--- app/src/main/java/com/example/nutriwise/LoginActivity.java
+++ app/src/main/java/com/example/nutriwise/LoginActivity.java
@@ -9,11 +9,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Toast;
+import android.view.View;
 
 
 public class LoginActivity extends AppCompatActivity {
     RadioGroup radioGroup;
-    Button buttonSign;
+    Button buttonSign, buttonalready;
     EditText editTextName,editTextEmail,editTextPhone,editTextAge,editTextWeight,editTextHeight;
     String user_name,user_mail,user_Phone,user_age,user_weight,user_height, user_genderString;
 
@@ -37,71 +38,76 @@ public class LoginActivity extends AppCompatActivity {
         init();
     }
 
-    public void init()
-    {
-        editTextName= findViewById(R.id.editTextName);
-        editTextEmail= findViewById(R.id.editTextEmail);
-        editTextPhone= findViewById(R.id.editTextPhone);
-        editTextAge= findViewById(R.id.editTextAge);
-        user_genderString=" ";
-        editTextHeight= findViewById(R.id.editTextHeight);
-        editTextWeight= findViewById(R.id.editTextWeight);
-        radioGroup= findViewById(R.id.radioGroup);
+    public void init() {
+        editTextName = findViewById(R.id.editTextName);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPhone = findViewById(R.id.editTextPhone);
+        editTextAge = findViewById(R.id.editTextAge);
+        user_genderString = " ";
+        editTextHeight = findViewById(R.id.editTextHeight);
+        editTextWeight = findViewById(R.id.editTextWeight);
+        radioGroup = findViewById(R.id.radioGroup);
         radioGroup.setOnCheckedChangeListener(((group, checkedId) ->
         {
-            if (checkedId==R.id.male)
-            {
-                user_genderString="male";
-                Log.d(TAG ,"male selected"+user_genderString);
-            }else if(checkedId==R.id.female)
-            {
-                user_genderString="female";
-                Log.d(TAG,"female selected"+ user_genderString);
+            if (checkedId == R.id.male) {
+                user_genderString = "male";
+                Log.d(TAG, "male selected" + user_genderString);
+            } else if (checkedId == R.id.female) {
+                user_genderString = "female";
+                Log.d(TAG, "female selected" + user_genderString);
             }
         }
-                ));
+        ));
+        buttonalready=findViewById(R.id.buttonalready);
+        buttonSign = findViewById(R.id.buttonSign);
+        buttonSign.setOnClickListener((view) -> {
 
-        buttonSign= findViewById(R.id.buttonSign);
-        buttonSign.setOnClickListener((view) ->{
-
-            user_name=editTextName.getText().toString().trim();
-            user_mail=editTextEmail.getText().toString().trim();
-            user_Phone=editTextPhone.getText().toString().trim();
-            user_height=editTextHeight.getText().toString().trim();
-            user_weight=editTextWeight.getText().toString().trim();
-            user_age=editTextAge.getText().toString().trim();
-            if (user_name.equalsIgnoreCase(" "))
-            {
-                Toast.makeText(LoginActivity.this,fill_user_name,Toast.LENGTH_SHORT).show();
-            }
-            else if(user_mail.equalsIgnoreCase(" "))
-            {
-                Toast.makeText(LoginActivity.this,fill_user_mail,Toast.LENGTH_SHORT).show();
-            }
-            else if(user_Phone.equalsIgnoreCase(" "))
-            {
-                Toast.makeText(LoginActivity.this,fill_user_phone,Toast.LENGTH_SHORT).show();
-            }
-            else if(user_Phone.length()!=10)
-            {
-                Toast.makeText(LoginActivity.this,fill_user_phone_invalid,Toast.LENGTH_SHORT).show();
-            }
-            else if(user_weight.equalsIgnoreCase(" "))
-            {
-                Toast.makeText(LoginActivity.this,fill_user_weight,Toast.LENGTH_SHORT).show();
-            }
-            else if(user_height.equalsIgnoreCase(" "))
-            {
-                Toast.makeText(LoginActivity.this,fill_user_height,Toast.LENGTH_SHORT).show();
-            }
-            else if(user_age.equalsIgnoreCase(" "))
-            {
-                Toast.makeText(LoginActivity.this,fill_user_age,Toast.LENGTH_SHORT).show();
+            user_name = editTextName.getText().toString().trim();
+            user_mail = editTextEmail.getText().toString().trim();
+            user_Phone = editTextPhone.getText().toString().trim();
+            user_height = editTextHeight.getText().toString().trim();
+            user_weight = editTextWeight.getText().toString().trim();
+            user_age = editTextAge.getText().toString().trim();
+            if (user_name.equalsIgnoreCase(" ")) {
+                Toast.makeText(LoginActivity.this, fill_user_name, Toast.LENGTH_SHORT).show();
+            } else if (user_mail.equalsIgnoreCase(" ")) {
+                Toast.makeText(LoginActivity.this, fill_user_mail, Toast.LENGTH_SHORT).show();
+            } else if (user_Phone.equalsIgnoreCase(" ")) {
+                Toast.makeText(LoginActivity.this, fill_user_phone, Toast.LENGTH_SHORT).show();
+            } else if (user_Phone.length() != 10) {
+                Toast.makeText(LoginActivity.this, fill_user_phone_invalid, Toast.LENGTH_SHORT).show();
+            } else if (user_weight.equalsIgnoreCase(" ")) {
+                Toast.makeText(LoginActivity.this, fill_user_weight, Toast.LENGTH_SHORT).show();
+            } else if (user_height.equalsIgnoreCase(" ")) {
+                Toast.makeText(LoginActivity.this, fill_user_height, Toast.LENGTH_SHORT).show();
+            } else if (user_age.equalsIgnoreCase(" ")) {
+                Toast.makeText(LoginActivity.this, fill_user_age, Toast.LENGTH_SHORT).show();
             }
         });
-        Intent i = new Intent(LoginActivity.this, AlreadyUser.class);
-        startActivity(i);
-        finish();
+        buttonSign.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+                Intent i = new Intent(LoginActivity.this, DashBoard.class);
+                startActivity(i);
+                finish();
+            }
+
+        });
+        buttonalready.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(LoginActivity.this,AlreadyUser.class);
+                startActivity(in);
+                finish();
+            }
+        });
+
+
+
     }
 
-}
+    }
+
